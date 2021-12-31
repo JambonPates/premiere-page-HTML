ff3cf57ac849b7c8695bea6389e499eb8d556003
anatoleNSI <91608984+anatoleNSI@users.noreply.github.com>
2021-12-31 18:57:48 +0100

    avancées java script

--- javascript/boutons.js
+++
@@ -1,8 +0,0 @@
-function mouse_on_button(element) {
-    element.style.height = '250px';
-    element.style.padding = '35px';
-}
-function mouse_leave_button(element) {
-    element.style.height = '200px';
-    element.style.padding = '60px';
-}
--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,29 @@
-var page = document.getElementById('page');
-console.log(page);
-console.log(page.style);
-fondcouleur  =page.style.backgroundColor = "red";
-bouton = fondcouleur
+function mouse_on_button(element) {
+    element.style.height = '220px';
+    element.style.padding = '45px';
+}
+function mouse_leave_button(element) {
+    element.style.height = '200px';
+    element.style.padding = '60px';
+}
+
+// reponse question
+
+let levote = document.getElementById("zonevote");
+
+zonevote.addEventListener("submit", function(e){
+    
+    let input = document.getElementById("levote");
+    if (input.value.trim() == ""){
+        let monErreur = document.getElementById("outputvote");
+        monErreur.innerHTML = "Erreur";
+        monErreur.style.color = "green";
+        e.preventDefault();
+    } else if (input.value.trim() == "oui"){
+
+    }
+    
+}
+
+
+
