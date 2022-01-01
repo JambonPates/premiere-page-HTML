72882ce46d3f50ef25b1fce2d5e82b1b2200b019
anatoleNSI <91608984+anatoleNSI@users.noreply.github.com>
2022-01-01 20:25:50 +0100

    mise à jour majeur des différentes pages
    ajout de contenu dans les tableaux,
    ajout du mode sombre et mode clair,
    système de vote terminé

--- javascript/script.js
+++ javascript/script.js
@@ -7,23 +7,54 @@ function mouse_leave_button(element) {
     element.style.padding = '60px';
 }
 
-// reponse question
-
-let levote = document.getElementById("zonevote");
-
-zonevote.addEventListener("submit", function(e){
-    
-    let input = document.getElementById("levote");
-    if (input.value.trim() == ""){
-        let monErreur = document.getElementById("outputvote");
-        monErreur.innerHTML = "Erreur";
-        monErreur.style.color = "green";
-        e.preventDefault();
-    } else if (input.value.trim() == "oui"){
-
+function vote (){
+    var input = document.querySelector('#inputvote').value;
+    if (input == 'oui') {
+        document.body.style.backgroundColor = 'rgb(0, 150, 0)';
+        var output = document.querySelector('#outputvote').textContent = '   ===>  Bonne réponse  <===';
+        var output = document.querySelector('#outputvote').style.color = 'rgb(0, 255, 0)';
+    } else if (input == 'non') {
+    document.body.style.backgroundColor = 'rgb(200, 0, 0)'
+        var output = document.querySelector('#outputvote').textContent = '   ===>  Mauvaise réponse  <===';
+        var output = document.querySelector('#outputvote').style.color = 'rgb(255, 0, 0)';
+    } else if (input == '') {
+    document.body.style.backgroundColor = 'rgb(0, 0, 255)'
+        var output = document.querySelector('#outputvote').textContent = '   ===>  Veuillez écrire votre réponse  <===';
+        var output = document.querySelector('#outputvote').style.color = 'rgb(0, 0, 0)';
+    } else {
+    document.body.style.backgroundColor = 'rgb(0, 0, 255)'
+        var output = document.querySelector('#outputvote').textContent = '   ===>  Cette réponse ne peut pas être acceptée  <===';
+        var output = document.querySelector('#outputvote').style.color = 'rgb(0, 0, 0)';
     }
-    
 }
 
-
-
+function modeSombre(){
+    var color = document.body.style.backgroundColor;
+    if (color = 'rgb(153, 153, 153)') {
+        document.body.style.backgroundColor = 'rgb(20, 20, 20)'
+        var output = document.querySelector('#couleurBan').style.backgroundColor = 'rgb(100, 100, 100)';
+        var output = document.querySelector('#basPage').style.backgroundColor = 'rgb(100, 100, 100)';
+        var output = document.querySelector('#ligne').style.backgroundColor = 'rgb(100, 100, 100)';
+        var output = document.querySelector('#pteban').style.backgroundColor = 'rgb(0, 0, 80)';
+        var output = document.querySelector('#pteban2').style.backgroundColor = 'rgb(0, 0, 80)';
+        var output = document.querySelector('#pteban3').style.backgroundColor = 'rgb(0, 0, 80)';
+        var output = document.querySelector('#pteban4').style.backgroundColor = 'rgb(0, 0, 80)'
+        var output = document.querySelector('#pteban5').style.backgroundColor = 'rgb(0, 0, 80)';
+        // var output = document.querySelector('#ban').style.background = 
+    }
+   
+}
+function modeClair(){
+    var color = document.body.style.backgroundColor;
+    if (color = 'rgb(0, 0, 0)') {
+        document.body.style.backgroundColor = 'rgb(153, 153, 153)'
+        var output = document.querySelector('#couleurBan').style.backgroundColor = 'rgb(0, 170, 145)';
+        var output = document.querySelector('#basPage').style.backgroundColor = 'rgb(0, 170, 145)';
+        var output = document.querySelector('#ligne').style.backgroundColor = 'rgb(0, 170, 145)';
+        var output = document.querySelector('#pteban').style.backgroundColor = 'rgb(10, 0, 130)';
+        var output = document.querySelector('#pteban2').style.backgroundColor = 'rgb(10, 0, 130)';
+        var output = document.querySelector('#pteban3').style.backgroundColor = 'rgb(10, 0, 130)';
+        var output = document.querySelector('#pteban4').style.backgroundColor = 'rgb(10, 0, 130)'
+        var output = document.querySelector('#pteban5').style.backgroundColor = 'rgb(10, 0, 130)';
+    }
+}
